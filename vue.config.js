6e73ef42a50b640fd246facd619ac46de193b183
许进 <gxujin@163.com>
2022-08-05 12:43:55 +0800

    cssmk,升级sass版本，解决图标乱码

--- vue.config.js
+++ vue.config.js
@@ -50,6 +50,15 @@ module.exports = {
       }
     }
   },
+  css: {
+    loaderOptions: {
+      sass: {
+        sassOptions: {
+          outputStyle: 'expanded'
+        }
+      }
+    }
+  },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
