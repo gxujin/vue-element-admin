7fb553c5b41941a58ce998ac183f21c8ecd6c904
gxujin <gxujin@163.com>
2022-12-03 12:28:29 +0800

    markdown示例暂时删除，tui-editor安装报错

--- src/components/MarkdownEditor/index.vue
+++ src/components/MarkdownEditor/index.vue
@@ -3,10 +3,12 @@
 </template>
 
 <script>
+// deps for editor
+import 'codemirror/lib/codemirror.css' // codemirror
+import 'tui-editor/dist/tui-editor.css' // editor ui
+import 'tui-editor/dist/tui-editor-contents.css' // editor content
 
-import 'codemirror/lib/codemirror.css'
-import '@toast-ui/editor/dist/toastui-editor.css'
-import Editor from '@toast-ui/editor'
+import Editor from 'tui-editor'
 import defaultOptions from './default-options'
 
 export default {
@@ -60,8 +62,8 @@ export default {
   },
   watch: {
     value(newValue, preValue) {
-      if (newValue !== preValue && newValue !== this.editor.getMarkdown()) {
-        this.editor.setMarkdown(newValue)
+      if (newValue !== preValue && newValue !== this.editor.getValue()) {
+        this.editor.setValue(newValue)
       }
     },
     language(val) {
@@ -88,10 +90,10 @@ export default {
         ...this.editorOptions
       })
       if (this.value) {
-        this.editor.setMarkdown(this.value)
+        this.editor.setValue(this.value)
       }
       this.editor.on('change', () => {
-        this.$emit('input', this.editor.getMarkdown())
+        this.$emit('input', this.editor.getValue())
       })
     },
     destroyEditor() {
@@ -100,10 +102,10 @@ export default {
       this.editor.remove()
     },
     setValue(value) {
-      this.editor.setMarkdown(value)
+      this.editor.setValue(value)
     },
     getValue() {
-      return this.editor.getMarkdown()
+      return this.editor.getValue()
     },
     setHtml(value) {
       this.editor.setHtml(value)
